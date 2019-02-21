86b150ad312b468c4e8747000cecb32008b66322
Akshit Patel <akshitpatel72@gmail.com>
2019-02-21 14:59:14 +0530

    click event added(basic)

--- quiz.js
+++ quiz.js
@@ -1,7 +1,27 @@
+var currentSelected = 0; 
+function getText(element){
+    var text = element.innerText;
+    return text;
+}
+window.addEventListener("click", function(){
+    if(isClicked){
+        isClicked= false;
+    }
+});
+function getElement(text){
+    var element= document.querySelector(".questions-container .questions:nth-child("+text+")");
+    return element;
+}
+
 function questionDisplay(content){
     var newElement = document.createElement("div");
     newElement.className = "questions";
     var questionsContainer = document.getElementsByClassName("questions-container")[0];
+    newElement.addEventListener("click", function(){
+        var text =  getText(this);
+        var element = getElement(text);
+        element.className= "items attempted"; 
+    });
     newElement.innerHTML= content;
     questionsContainer.appendChild(newElement);
 }
