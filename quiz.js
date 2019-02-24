5c0b537bd2c741ca1440c7bf8dfc766117e880be
Akshit Patel <akshitpatel72@gmail.com>
2019-02-25 00:17:08 +0530

    adding markers for questions(basic)

--- quiz.js
+++ quiz.js
@@ -3,16 +3,46 @@ function getText(element){
     var text = element.innerText;
     return text;
 }
-window.addEventListener("click", function(){
-    if(isClicked){
-        isClicked= false;
-    }
-});
+var isIndicatorSelected = false;
+var indicatorSelected;
+// window.addEventListener("click", function(){
+//     if(isIndicatorSelected){
+//         isIndicatorSelected= false;
+//     }
+// });
 function getElement(text){
     var element= document.querySelector(".questions-container .questions:nth-child("+text+")");
     return element;
 }
-
+var indicatorList = document.querySelectorAll(".indicators");
+function addBorder(indicator){
+    indicator.className+= " indicator-border";
+};
+function removeBorder(indicator){
+    indicator.className= "indicators";
+};
+function addClickEvent(indicator){
+indicatorList[i].addEventListener("click",function(){
+    if(isIndicatorSelected == false){
+        isIndicatorSelected = true;
+        addBorder(indicator);
+        
+        indicatorSelected= indicator.children[0].className;
+        console.log(isIndicatorSelected);
+        console.log(indicatorSelected);
+    } else{
+        isIndicatorSelected = false; 
+        removeBorder(indicator);
+        console.log(isIndicatorSelected);
+        
+        console.log(isIndicatorSelected);  
+    }
+ });
+}
+for(var i=0; i<indicatorList.length;i++){
+    console.log(indicatorList[i]);
+    addClickEvent(indicatorList[i]);
+}
 function questionDisplay(content){
     var newElement = document.createElement("div");
     newElement.className = "questions";
@@ -20,13 +50,54 @@ function questionDisplay(content){
     newElement.addEventListener("click", function(){
         var text =  getText(this);
         var element = getElement(text);
-        element.className= "items attempted"; 
+        console.log(isIndicatorSelected);
+        console.log(indicatorSelected);
+        if(isIndicatorSelected){
+            if(indicatorSelected== "items attempted"){
+                element.className= "items attempted";
+            }
+            else if(indicatorSelected== "items attempted"){
+                element.className= "items attempted";
+            }
+            else if(indicatorSelected== "items not-attempted"){
+                element.className= indicatorSelected;
+            }
+            else if(indicatorSelected== "items to-be-reviewed"){
+                element.className= indicatorSelected;
+            }
+            else if(indicatorSelected== "items not-reviewed-yet"){
+                element.className = indicatorSelected;
+            }
+            else{
+                element.className = indicatorSelected;
+            }
+        }
     });
     newElement.innerHTML= content;
     questionsContainer.appendChild(newElement);
+}
+function addBorder(indicator){
+    indicator.classList.toggle("indicator-border");
 }
 
 var numOfQuestions = 20;
 for(var i= 1; i<=numOfQuestions ; i++){
     questionDisplay(i);
 }
+
+// function XML_HTTP(){
+//     var input = document.querySelector("input").value;
+//     var request = new XMLHttpRequest();
+//     request.open('GET', `https://api.github.com/users/${input}`, true);
+    
+//     request.onload = function () {
+//         var data = JSON.parse(this.response);
+//         if (request.status >= 200 && request.status < 400) {
+//             represent_data(data);
+//         } else {
+//             console.log('error');
+//         }
+//     }
+//     request.send();
+    
+//     }
