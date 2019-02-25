2e5bf6853063fe980f5381585c82a6a317711ddf
Shreyans Jain <shreyansj551@gmail.com>
2019-02-25 10:49:25 +0530

    Nav menu implemented, content to be added

--- quiz.js
+++ quiz.js
@@ -10,3 +10,8 @@ var numOfQuestions = 20;
 for(var i= 1; i<=numOfQuestions ; i++){
     questionDisplay(i);
 }
+
+const nav = document.querySelector(".nav-menu");
+
+document.querySelector(".ham").addEventListener("click", () => {nav.style.left = "0"});
+document.querySelector("#close-nav").addEventListener("click", () => {nav.style.left = "-100%"});
